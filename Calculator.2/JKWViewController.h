7a797b1c99797d75dfa392731f0696ba2debfa51
Jennifer K Waldner <jenniferkwaldner@gmail.com>
2014-05-19 20:07:12 -0400

    Initial Commit

---
+++ Calculator.2/JKWViewController.h
@@ -0,0 +1,13 @@
+//
+//  JKWViewController.h
+//  Calculator.2
+//
+//  Created by Jennifer Waldner on 5/19/14.
+//  Copyright (c) 2014 Jennifer K Design, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKWViewController : UIViewController
+
+@end
---
+++ Calculator.2Tests/Calculator_2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Calculator_2Tests.m
+//  Calculator.2Tests
+//
+//  Created by Jennifer Waldner on 5/19/14.
+//  Copyright (c) 2014 Jennifer K Design, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Calculator_2Tests : XCTestCase
+
+@end
+
+@implementation Calculator_2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
