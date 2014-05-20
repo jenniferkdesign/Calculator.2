3dfbee77b3f8060119a79dbe13c885cb22aae452
Jennifer K Waldner <jenniferkwaldner@gmail.com>
2014-05-19 22:04:59 -0400

    connecting buttons

--- Calculator.2/JKWViewController.m
+++ Calculator.2/JKWViewController.m
@@ -14,6 +14,56 @@
 
 @implementation JKWViewController
 
+-(IBAction)number1:(id)sender{
+    
+}
+-(IBAction)number2:(id)sender{
+    
+}
+-(IBAction)number3:(id)sender{
+    
+}
+-(IBAction)number4:(id)sender{
+    
+}
+-(IBAction)number5:(id)sender{
+    
+}
+-(IBAction)number6:(id)sender{
+    
+}
+-(IBAction)number7:(id)sender{
+    
+}
+-(IBAction)number8:(id)sender{
+    
+}
+-(IBAction)number9:(id)sender{
+    
+}
+-(IBAction)number0:(id)sender{
+    
+}
+-(IBAction)times:(id)sender{
+    
+}
+-(IBAction)divide:(id)sender{
+    
+}
+-(IBAction)plus:(id)sender{
+    
+}
+-(IBAction)subtract:(id)sender{
+    
+}
+-(IBAction)clear:(id)sender{
+    
+}
+-(IBAction)equals:(id)sender{
+    
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
